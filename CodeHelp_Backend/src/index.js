c4158e09d098b9fe30b994b97d797ddbdb315d25
Darshan Yogesh Patil <darshanpatil135790@gmail.com>
2024-11-02 11:09:02 +0530

    Add user router and update server response handling in index.js

--- CodeHelp_Backend/src/index.js
+++ CodeHelp_Backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from 'dotenv';
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -7,10 +8,26 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Learning Express From CodeHelp")
-})
+app.use("/api", userRouter);
+
+
+// We wirte all request in routes folder and import here this is industry standard
+// app.get('/', (req, res) => {
+//     res.send("Learning Express From CodeHelp,  Got a GET request")
+// })
+
+// app.post('/sign-in', (req, res) => {
+//     res.send("Got a POST request")
+// })
+
+// app.put('/update-profile', (req, res) => {
+//     res.send("Got a PUT request")
+// })
+
+// app.delete('/delete-profile', (req, res) => {
+//     res.send("Got a DELETE request")
+// })
 
 app.listen(port, () => {
-    console.log(`Server is running on port http://127.0.0.1:${port}/`)
+    console.log(`Server is running on port http://localhost:${port}/`)
 })
